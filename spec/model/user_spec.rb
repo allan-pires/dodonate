1d14db189fb6f675d9f3bde02ca29a0cb781cbcc
allan.victor <doislan@gmail.com>
2018-04-28 18:24:26 -0300

    Adds rspec gem and user model test

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe User do
+  let (:user) { User.new(name: "Adalberto", email: "adalberto@gmail.com") }
+
+  describe ".valid?" do
+  	
+  	context "with valid information" do
+  	  it { expect(user.valid?).to be_truthy }
+  	end
+
+    context "with empty email" do
+      before do
+      	user.email = ""
+      end
+	  
+	  it { expect(user.valid?).to be_falsey }
+	end
+
+	context "with email starting with symbol" do
+      before do
+      	user.email = "$hue@gmail.com"
+      end
+	  
+	  it { expect(user.valid?).to be_falsey }
+	end
+
+	context "with email without @" do
+      before do
+      	user.email = "huegmail.com"
+      end
+	  
+	  it { expect(user.valid?).to be_falsey }
+	end
+
+	context "with email without .com or .anything" do
+      before do
+      	user.email = "hue@@gmail"
+      end
+	  
+	  it { expect(user.valid?).to be_falsey }
+	end
+
+  end  
+end
