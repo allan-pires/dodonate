3bdf968c709009a8b0de3e5c47e4e327bd3aa21c
allan.victor <doislan@gmail.com>
2018-04-28 22:24:08 -0300

    Add user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+    if @user.save
+	  flash[:success] = "Hey! it's nice to have you here"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get  '/home', to: 'static_pages#home'
   get  '/help', to: 'static_pages#help'
   get  '/about', to: 'static_pages#about'
-  get 'users/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
