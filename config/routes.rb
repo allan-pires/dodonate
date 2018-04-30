daed0b414ef066467f6a87a031acf60d4fe359e9
allan.victor <doislan@gmail.com>
2018-04-30 01:35:56 -0300

    Identation and remove unnused variable

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/home', 			to: 'static_pages#home'
-  get '/about', 		to: 'static_pages#about'
+  get '/home', 			    to: 'static_pages#home'
+  get '/about', 		    to: 'static_pages#about'
   
-  get '/signup', 		to: 'users#new'
+  get '/signup', 		    to: 'users#new'
   get '/profile/edit', 	to: 'users#edit'
-  get '/profile', 		to: 'users#show'
+  get '/profile', 		  to: 'users#show'
   
-  get '/login', 		to: 'sessions#new'
-  post '/login', 		to: 'sessions#create'
-  delete '/logout', 	to: 'sessions#destroy'
+  get '/login', 		    to: 'sessions#new'
+  post '/login', 		    to: 'sessions#create'
+  delete '/logout', 	  to: 'sessions#destroy'
 
-  get '/item/donate', to: 'items#new'
-  get 'item/edit', to: 'items#edit'
+  get '/item/donate',   to: 'items#new'
+  get '/item/edit',     to: 'items#edit'
   
   resources :users
   resources :items
