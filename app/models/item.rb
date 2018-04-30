8f60cda670dd54089e1b25186a61d88e02352f04
allan.victor <doislan@gmail.com>
2018-04-30 17:23:06 -0300

    Create item model test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :item_category
+
+  validates :name, 
+    presence: true, 
+    length: { maximum: 255 }
+  
+  validates :quantity, 
+    presence: true 
+
+  validates :item_category, 
+    presence: true
+
+  validates :user, 
+    presence: true
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,48 @@
-require 'rails_helper'
+require "spec_helper"
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+  let (:item) { create(:valid_item) }
+
+  describe ".valid?" do
+    
+    context "with valid information" do
+      it { expect(item.valid?).to be_truthy }
+    end
+
+    context "without name" do
+      before do 
+        item.name = nil 
+      end
+      it { expect(item.valid?).to be_falsey }
+    end
+
+    context "without description" do
+      before do 
+        item.description = nil 
+      end
+      it { expect(item.valid?).to be_truthy }
+    end
+
+    context "without quantity" do
+      before do 
+        item.name = nil 
+      end
+      it { expect(item.valid?).to be_falsey }
+    end
+
+    context "without category" do
+      before do 
+        item.item_category = nil 
+      end
+      it { expect(item.valid?).to be_falsey }
+    end
+
+    context "without owner (user)" do
+      before do 
+        item.user = nil 
+      end
+      it { expect(item.valid?).to be_falsey }
+    end
+
+  end
 end
