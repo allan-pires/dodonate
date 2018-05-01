9a6862869dc2bd87d0b80c66db4ce94d41719ef2
allan.victor <doislan@gmail.com>
2018-05-01 03:23:40 -0300

    Add test coverage badge

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :test do
   gem 'selenium-webdriver'
   gem 'chromedriver-helper'
   gem 'simplecov', require: false
+  gem 'simplecov-shield'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
+require 'simplecov-shield'
 
 Rails.logger = Logger.new(STDOUT)
+SimpleCov.formatter = SimpleCov::Formatter::ShieldFormatter
 
 # Initialize the Rails application.
 Rails.application.initialize!
