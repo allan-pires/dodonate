04fe27c4231ab54a45de1042edad09294ab86558
allan.victor <doislan@gmail.com>
2018-05-01 15:41:46 -0300

    Add check for cookies to avoid nullpointer when accessing it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    logger.info("Creating new session")
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+    logger.info("Logging user [id = #{user.id}, name = #{user.name}]")
       log_in user
       redirect_to home_path
     else
+      logger.info("Authentication failed")
       flash[:danger] = 'Invalid email or password :('
       render 'new'
     end
   end
 
   def destroy
+    logger.info("Logging out user [id = #{current_user.id}, name = #{current_user.name}] ")
     log_out
     redirect_to home_path
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,12 @@ module SessionsHelper
 
   def log_out
   	session.delete(:user_id)
-    @current_user = nil
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if !session[:user_id].nil?
+      User.find(session[:user_id])
+    end
   end
 
   def logged_in?
